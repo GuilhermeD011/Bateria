b45675fd1a87f33dc556ed9f66a1e58a0091621a
Guilherme Duarte <contatoduart9@gmail.com>
2024-07-30 22:48:22 -0300

    atualizando js

--- script.js
+++ script.js
@@ -1,56 +1,48 @@
 // evento de click em toda minha aplicacao, cada letra da bateria sera tocavel apertando cada tecla da mesma
-document.body.addEventListener('keyup', function(event){
-    playsound(event.code.toLowerCase())
-})
-
+document.body.addEventListener('keyup', (event)=>{
+    playSound( event.code.toLowerCase() );
+});
 
 // adicao de click no meu botao 'tocar'
-document.querySelector('.composer button').addEventListener('click', function(){
-    let song = document.querySelector('#input').value 
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('#input').value;
 
-   
     // transformando meu comando de toque da bateria em um array para ordernar quais sons tocarao
-    if(song !== ''){
-        let songArray = song.split('')
-        playcomposicao(songArray);
+    if(song !== '') {
+        let songArray = song.split('');
+        playComposition(songArray);
     }
+});
 
-})
+function playSound(sound) {
+    let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
 
-function playsound(sound){
-    let audioElement = document.querySelector(`#s_${sound}`)
-    let keyElement = document.querySelector(`div[data-key="${sound}"]`)
-
-    if(audioElement){
+    
+    if(audioElement) {
         // o meu elemento de audio ira zerar antes mesmo de terminar
         audioElement.currentTime = 0;
         // apos a acao anterior ele tocara
         audioElement.play();
     }
 
-    if(keyElement){
-
+    if(keyElement) {
         keyElement.classList.add('active');
-
-
         // tempo de retirada de audio 
         setTimeout(()=>{
-            keyElement.classList.remove('active')
-        }, 100)
+            keyElement.classList.remove('active');
+        }, 300);
     }
 }
 
+function playComposition(songArray) {
+    let wait = 0;
 
-function playcomposicao(songArray){
-    let wait = 0 
-    for(let songItem of songArray){
-        setTimeout(() => {
-        playsound(`key${songItem}`);
+    for(let songItem of songArray) {
+        setTimeout(()=>{
+            playSound(`key${songItem}`);
         }, wait);
+
         wait += 250;
-        
     }
 }
-
-
-
